226ea90e566d348df08e204e9c505255d1809d20
hammarskiold <patrik.hammarskiold@gmail.com>
2023-09-25 14:57:04 +0200

    add new api key

--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const transportMode = "BUS";
-const apiKey = "a002c1fe27094748843a708187440bfa";
+const apiKey = "15283e2f8ba0489d825f38bcdb1541c8";
 const baseUrl = "https://api.sl.se/api2/LineData.json";
 
 const url = `${baseUrl}?DefaultTransportModeCode=${transportMode}&key=${apiKey}`;
